7f87cc00d3d7905cdf11acca55a5ffed91849877
Alison-Azman <aazman@student.touro.edu>
2026-02-01 18:35:47 -0500

    Update About_Me_Github.py

--- About_Me_Github.py
+++ About_Me_Github.py
@@ -1,6 +1,7 @@
 
 
 #Hi my name is Alison Azman
+#Module 1 FQ Class 1
 #About me
 
 live="Baltimore"
@@ -10,3 +11,4 @@ nickname="Reizi"
 job="I work in a school that mainly caters to children with special needs,\nand I do the insurance authorizations for them."
 
 print(f"Hi, my name is {nickname}! I live in {live} and I am going for {degree}.\nDuring the day, from 9-4, {job} \nI am excited to learn more coding skills.")
+
